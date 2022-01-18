2f7fabe61ef58076701b6245fb501f1310948860
Nathan <usmc_wolf@hotmail.com>
2022-01-18 12:39:02 -0600

    Initial Commit Api for Product Storage

---
+++ app.py
@@ -0,0 +1,130 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+CORS(app)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Picture(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    item_title = db.Column(db.String, nullable=False)
+    category = db.Column(db.String)
+    item_img = db.Column(db.String, unique=True)
+
+    def __init__(self, item_title, category, item_img):
+        self.item_title = item_title
+        self.category = category
+        self.item_img = item_img
+
+class PictureSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'item_title', 'category', 'item_img')
+
+picture_schema = PictureSchema()
+multi_picture_schema = PictureSchema(many=True)
+
+# /////////////////// How to Add items to the Database \\\\\\\\\\\\\\\
+@app.route('/item/add', methods=["POST"])
+def add_item():
+    if request.content_type != 'application/json':
+        return jsonify('Error: Data must be sent as JSON')
+
+    post_data = request.get_json()
+    item_title = post_data.get('item_title')
+    category = post_data.get('category')
+    item_img = post_data.get('item_img')
+
+    if item_title == None:
+        return jsonify("Error: You must provide an 'Item Title' key")
+    if item_img == None:
+        return jsonify("Error: You must provide a 'Picture' key")
+
+    new_record = Picture(item_title, category, item_img)
+    db.session.add(new_record)
+    db.session.commit()
+
+    return jsonify(picture_schema.dump(new_record))
+
+# /////////////////// How to Get all items from the Database \\\\\\\\\\\\\\\
+
+@app.route('/item/get', methods=["GET"])
+def get_all_items():
+    all_records = db.session.query(Picture).all()
+    return jsonify(multi_picture_schema.dump(all_records))
+
+# /////////////////// How to Get items by ID from the Database \\\\\\\\\\\\\\\
+@app.route('/item/get/<id>', methods=["GET"])
+def get_item_id(id):
+    one_item = db.session.query(Picture).filter(Picture.id == id).first()
+    return jsonify(picture_schema.dump(one_item))
+
+
+# /////////////////// How to Update Items to the Database \\\\\\\\\\\\\\\
+@app.route('/item/update/<id>', methods=["PUT"])
+def update_item(id):
+    if request.content_type != 'application/json':
+        return jsonify("Error: Datamust be sent as JSON")
+
+    put_data = request.get_json()
+    item_title = put_data.get('item_title')
+    category = put_data.get('category')
+    item_img = put_data.get('item_img')
+
+    item_to_update = db.session.query(Picture).filter(Picture.id == id).first()
+
+    if item_title != None:
+        item_to_update.item_title = item_title
+    if category != None:
+        item_to_update.category = category
+    if item_img != None:
+        item_to_update.item_img = item_img
+
+    db.session.commit()
+
+    return jsonify(picture_schema.dump(item_to_update))
+
+    # /////////////////// How to Delete Items from the Database \\\\\\\\\\\\\\\
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
